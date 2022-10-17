fa4fd0f37b8e8ef2f82945fa96fca7fe26c93e2e
Tressie Muthuri <tressiemuthuri@gmail.com>
2022-10-17 12:43:51 +0300

    Completed assignment

---
+++ app.rb
@@ -0,0 +1,30 @@
+puts "Hello World!"
+print "Pass this test, please."
+# print "Hello world!"
+# #puts=consolelog(). using the parenthesis in Ruby is optional
+# #puts, print, pp, p
+# #puts inserts a line break print does not
+# #puts and print are best for strings
+
+# puts("Hello world!")
+
+# # commenting in Ruby = #
+
+# puts [1,2,3]
+# #puts calls the .to_s method
+p [1,2,3]
+# #p calls the .inspect method
+
+# #pp is for pretty printing complex data like nested arrays & hashes.
+# #pp calls the   .pretty_inspect
+
+# pp [
+#     { id: 1, hello: "World" }, 
+#     { id: 2, hello: "Ruby" }, 
+#     { id: 3, hello: "Moon" }, 
+#     { id: 4, hello: "Learner" }]
+
+# #IRB Interactive Ruby is for running Ruby REPL(Read, Evaluate, Print, Loop)
+# #(in JS) null = nil (in Ruby) || no value
+# #the puts method always returns a nil
+
